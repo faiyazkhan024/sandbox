077f7b8a2f5d5f30a76e5eb5b6c29d38c6d7da1d
Faiyaz Khan <faiyazkhan024@gmail.com>
2024-01-09 20:04:23 +0530

    Seperated html in components

--- hailcabs-coding-challenge/challenge-1/include.js
+++
@@ -1,7 +0,0 @@
-[...document.getElementsByTagName("include")].forEach(async (element) => {
-  try {
-    element.innerHTML = await await fetch(element.getAttribute("src"));
-  } catch {
-    element.innerHTML = "No such File or Directory is Found";
-  }
-});
