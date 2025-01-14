8c4278296cacda1343679c895c40b38fc3c2bc68
Faiyaz khan <faiyazkhan024@gmail.com>
2025-01-14 13:33:43 +0530

    Add Cropper

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
+import Cropper, { ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
+import floorPlan from "./assets/floor-plan.png";
 import { Header } from "./components/Header/Header";
+import classes from "./App.module.css";
 
-const App: FC = () => {
-  return <Header setImgUrl={() => {}} />;
+export const App: FC = () => {
+  const cropperRef = useRef<ReactCropperElement>(null);
+  const [imgUrl, setImgUrl] = useState(floorPlan);
+
+  return (
+    <>
+      <Header setImgUrl={setImgUrl} />
+
+      <div className={classes.app}>
+        <main className={classes.canvas}>
+          <Cropper
+            ref={cropperRef}
+            src={imgUrl}
+            viewMode={1}
+            guides={false}
+            dragMode="move"
+            minCropBoxWidth={100}
+            minCropBoxHeight={100}
+            initialAspectRatio={5 / 2}
+            checkOrientation={false}
+          />
+        </main>
+      </div>
+    </>
+  );
 };
-
-export default App;
