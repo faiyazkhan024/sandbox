1b2d12751c71e73d95a6f5b320bb1a223b4eb9d5
Faiyaz khan <faiyazkhan024@gmail.com>
2025-02-12 17:16:44 +0530

    Add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+import banner from "./banner.png";
+import community from "./community.avif";
+import community1 from "./community-1.webp";
+import community2 from "./community-2.jpg";
+import community3 from "./community-3.jpg";
+import community4 from "./community-4.webp";
+import profile from "./profile.jpg";
+import spehreLogo from "./spehre-logo.png";
+
+export {
+  banner,
+  community,
+  community1,
+  community2,
+  community3,
+  community4,
+  profile,
+  spehreLogo,
+};
